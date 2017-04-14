cd672ae983a8e9de1603951c4e1b44bdc994ca85
wxy <wxy@Davids-iMac.local>
2017-04-14 14:33:35 +0800

    项目创建

---
+++ MyTestApp/MyTestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTestApp
+//
+//  Created by wxy on 2017/4/14.
+//  Copyright © 2017年 wxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTestApp/MyTestApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyTestApp
+//
+//  Created by wxy on 2017/4/14.
+//  Copyright © 2017年 wxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
